0d3a2d82b52a83c87fc14294ba15b158170fd80a
William Estrela <wel.estrela@gmail.com>
2019-11-28 15:57:08 -0300

    Terminando implementacoes

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 typedef struct{
     int matricula;
-    char *nome;
-    char *curso;
+    char nome[24];
+    char curso[24];
 }Info;
 
 typedef struct tree
@@ -78,38 +78,273 @@ int isInTree(Tree* t, int matricula) {
     return t->info->matricula == matricula || isInTree(t->dir, matricula) || isInTree(t->esq, matricula);
 }
 
+int buscaPorMatricula(Tree * t, Info * entrada){ // faz a busca na �rvore por matr�cula
+
+    if(t != NULL){
+        buscaPorMatricula(t->esq, entrada);
+
+        if(t->info->matricula == entrada->matricula){
+            printf("\nBusca por Matr�cula: ");
+            printf("Nome: %s | Matr�cula:%d | Curso:%s \n\n", t->info->nome, t->info->matricula, t->info->curso);
+            return 1;
+        }
+        buscaPorMatricula(t->dir, entrada);
+    }
+    else{
+        return 0;
+    }
+
+}
+Tree * remove_atual(Tree * atual){
+    Tree* no1;
+    Tree* no2;
+    if(atual->esq == NULL){
+        no2 = atual->dir;
+        free(atual);
+        return no2;
+    }
+
+    no1= atual;
+    no2 = atual->esq;
+    while(no2->dir != NULL){
+        no1 = no2;
+        no2 = no2->dir;
+    }
+
+    if(no1 != atual){
+        no1->dir = no2->esq;
+        no2->esq = atual->esq;
+    }
+
+    no2->dir = atual->dir;
+    free(atual);
+    return no2;
+}
+
+Tree* removeRaiz(Tree* r){
+
+    Tree *p, *q;
+    if(r->esq == NULL)//raiz n�o possui filho a esquerda
+        q = r->dir;//filho a direita passa a ser a raiz
+    else{
+        p = r; q = r->esq;
+        while(q->dir != NULL){//busca maior valor da direita
+            p = q; q = q->dir;
+        }
+        if(p != r){
+            q->esq = r->esq;//atualiza nova esquerda de q (raiz)
+            p->dir = NULL;//atualiza nova direita de p
+        }
+        q->dir = r->dir;//conecta a direita da �rvore na nova raiz
+    }
+
+    free(r);
+    return q;
+
+}
+
+int removerPorNome(Tree **t, Info* entrada, Tree **t_mat, int cont);
+int removerPorMatricula(Tree **t, int matricula, Tree **t_nom, int cont){ // remove o noh por matr�cula
+
+    if(t == NULL) return 0;
+    Tree* ant= NULL;
+    Tree* atual= *t;
+    while(atual != NULL){
+        if(matricula == atual->info->matricula){
+            cont++;
+            if(cont==1)
+                removerPorNome(&(*t_nom),atual->info,&(*t), cont);
+            if(atual == *t){
+                *t = removeRaiz(atual);
+            } else{
+                if(ant->dir == atual)
+                    ant->dir = remove_atual(atual);
+                else
+                    ant->esq = remove_atual(atual);
+            }
+            return 1;
+        }
+        ant = atual;
+        if(matricula > atual->info->matricula)
+            atual = atual->dir;
+        else
+            atual = atual->esq;
+    }
+
+}
+int removerPorNome(Tree **t, Info* entrada, Tree **t_mat, int cont){ // remove o noh por matr�cula
+
+    if(*t == NULL) return 0;
+    Tree* ant= NULL;
+    Tree* atual= *t;
+    while(atual != NULL){
+        if(strcmp(entrada->nome,atual->info->nome) == 0){
+            cont++;
+            if(cont==1)
+                removerPorMatricula(&(*t_mat),atual->info->matricula,&(*t), cont);
+
+            if(atual == *t){
+                *t = removeRaiz(atual);
+            } else{
+                if(ant->dir == atual)
+                    ant->dir = remove_atual(atual);
+                else
+                    ant->esq = remove_atual(atual);
+            }
+
+            return 1;
+        }
+        ant = atual;
+        if(strcmp(entrada->nome, atual->info->nome) > 1)
+            atual = atual->dir;
+        else
+            atual = atual->esq;
+    }
+
+}
+int buscaPorNome(Tree *t, Info *entrada){ // faz uma busca na �rvore por nome
+
+    if(t != NULL){
+        buscaPorNome(t->esq, entrada);
+
+        if(strcmp(t->info->nome,entrada->nome)==0){
+            printf("\nBusca por Nome: ");
+            printf("Nome:%s | Matricula:%d | Curso:%s \n\n", t->info->nome, t->info->matricula, t->info->curso);
+            return 1;
+        }
+
+        buscaPorNome(t->dir, entrada);
+    }
+    else{
+        return 0;
+    }
+}
+
+int opcao;
+Tree* arvoreMatricula = createTree();
+Tree* arvoreNome = createTree();
+char flag;
+char curso[100];
+char nome[100];
+int  matricula;
+int matriculaBusca;
+char nomeBusca[100];
+Info* entrada = (Info*) malloc(sizeof(Info));
+
+int menu(){
 
 
-int main(){
+    cout << "Trabalho de Estrutura de dados" <<endl;
+    cout << "1 - Inserir" <<endl;
+    cout << "2 - Buscar" <<endl;
+    cout << "3 - Imprimir" <<endl;
+    cout << "4 - Remover" <<endl;
+    cin >> opcao;
 
-    Tree* t = createTree();
-    char flag;
-    char curso[100];
-    char nome[100];
-    do{
-        Info* usuario1 = (Info*) malloc(sizeof(Info));
+    switch (opcao){
+        case 1:
+            do{
+                char pergunta;
 
-        cout << "Matricula" <<endl;
-        cin >> usuario1->matricula;
+                Info* usuario1 = (Info*) malloc(sizeof(Info));
 
-        cout << "Curso" <<endl;
-        scanf("%s", &curso );
+                cout << "Matricula" <<endl;
+                cin >> usuario1->matricula;
 
-        cout << "Nome" <<endl;
-        scanf("%s", &nome );
+                cout << "Curso" <<endl;
+                scanf("%s", &curso );
 
-        cout << "Deseja terminar ?  s/n" <<endl;
-        cin >> flag;
+                cout << "Nome" <<endl;
+                scanf("%s", &nome );
 
-        usuario1->curso = curso;
-        usuario1->nome = nome;
+                strcpy(usuario1->curso,curso);
+                strcpy(usuario1->nome,nome);
 
-        insereOrdenadoMatricula(&t, usuario1);
-    }while (flag == 'n');
+                insereOrdenadoMatricula(&arvoreMatricula, usuario1);
+                insereOrdenadoNome(&arvoreNome, usuario1);
 
-    showTree(t);
+                showTree(arvoreMatricula);
+                showTree(arvoreNome);
 
-    free(t);
+                cout << "Deseja terminar de inserir?  (s/n)" <<endl;
+                cin >> flag;
+            }while (flag == 'n');
+            menu();
+            break;
+        case 4:
+            char opcao2;
+            cout << "Você deseja remover por nome ou matricula (n/m)" <<endl;
+            cin >> opcao2;
+
+
+            if(opcao2 == 'n' || opcao2 == 'N'){
+                cout << "Digite o nome" <<endl;
+                scanf("%s", &nome);
+
+                strcpy(entrada->nome,nome);
+
+                if(removerPorNome(&arvoreNome,entrada, &arvoreMatricula, 0)){
+                    printf("\nAluno removido\n");
+                }else{
+                    printf("Nome n�o encontrado!\n");
+                }
+            }
+            else{
+                if(opcao2 == 'm' || opcao2 == 'm'){
+                    cout << "Digite a matricula" <<endl;
+                    scanf("%d", &matricula);
+                    entrada->matricula = matricula;
+                    if(removerPorMatricula(&arvoreMatricula,entrada->matricula, &arvoreNome, 0)){
+                        printf("\nAluno removido!\n");
+                    }else {
+                        printf("Matricula n�o encontrada!\n");
+                    }
+                }
+            }
+            printf("\n\nArvore ordenada por matricula: \n");
+            showTree(arvoreMatricula); printf("\n\n");
+            printf("Arvore ordenada por Nome: \n");
+            showTree(arvoreNome); printf("\n\n");
+            menu();
+            break;
+        case 2:
+            char busca;
+            cout << "Buscando por nome ou matricula ? (n/m)"  <<endl;
+            Info* usuarioBusca = (Info*) malloc(sizeof(Info));
+            cin >> busca;
+
+            if ( busca == 'n'){
+                cout << "Nome" <<endl;
+                scanf("%s", &nomeBusca );
+                strcpy(usuarioBusca->nome,nome);
+
+                buscaPorMatricula(arvoreNome, usuarioBusca);
+            }else{
+                if ( busca == 'm'){
+                    cout << "Matricula" <<endl;
+                    cin >> usuarioBusca->matricula;
+                    if(!buscaPorMatricula(arvoreMatricula, usuarioBusca)){
+                        printf("Matricula n encontrado\n");
+                    }
+
+                }else{
+                    cout << "escolha errada"<<endl;
+                    menu();
+                }
+            }
+            break;
+    }
+
+
+
+
+    showTree(arvoreMatricula);
+
+    free(arvoreMatricula);
 
     return 0;
+}
+
+int main() {
+    menu();
 }
